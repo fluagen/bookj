b15c73ad2564b486f7d0da68502920bac81ed65a
majie <majie@calis.edu.cn>
2016-04-04 01:08:57 +0800

    增加 三级导航树 实现

--- templates/default/assets/common.js
+++ templates/default/assets/common.js
@@ -1,5 +1,11 @@
-$('#my-affix').affix({
-  offset: {
-    top: $('#my-affix').offset().top
-  }
+$(document).ready(function() {
+    $('#my-affix').affix({
+        offset: {
+            top: $('#my-affix').offset().top,
+            bottom: function() {
+                return (this.bottom =
+                    $('.footer').outerHeight(true) + 40)
+            }
+        }
+    });
 });
