cf5ef5af20b01cb762856b0aa4813af6bcbeb442
majie <majie@calis.edu.cn>
2016-04-27 22:40:03 +0800

    fix reply author is null

--- controllers/reply.js
+++ controllers/reply.js
@@ -5,13 +5,13 @@ var EventProxy = require('eventproxy');
 exports.add = function(req, res, next) {
     var content = req.body.r_content;
     var topic_id = req.params.topic_id;
-
+var current_user = req.session.user;
     var ep = new EventProxy();
     ep.all('reply', function(reply) {
         res.redirect('/topic/' + topic_id);
     });
     ep.fail(next);
-    replyManager.newAndSave(content, topic_id, null, ep.done(function(reply) {
+    replyManager.newAndSave(content, topic_id, current_user._id, ep.done(function(reply) {
         topicManager.updateLastReply(topic_id, reply._id, function(err) {
             if (err) {
                 return next(err);
