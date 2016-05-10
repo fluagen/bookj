3553d63bba41f38533eb31dfb3ee8ad7cf8f1c0c
majie <maj@calis.edu.cn>
2016-05-10 19:02:23 +0800

    update reply controller

--- controllers/reply.js
+++ controllers/reply.js
@@ -1,47 +1,38 @@
 var topicManager = require('../managers/topic');
 var replyManager = require('../managers/reply');
+var userManager = require('../managers/user');
 var EventProxy = require('eventproxy');
 var at = require('../common/at');
 
 exports.add = function(req, res, next) {
     var content = req.body.r_content;
     var topic_id = req.params.tid;
+    var cu = req.session.user;
     var ep = new EventProxy();
-    var current_user = req.session.user;
-
     ep.fail(next);
 
-
-    ep.all('topic', 'linkUsers', function(topic, linkedContent) {
-        if (!topic || !topic.author) {
+    ep.all('topic', function(topic) {
+        if (!topic) {
             return res.status(403).send('此话题不存在或已被删除。');
         }
-        replyManager.newAndSave(linkedContent, topic_id, current_user._id, ep.done(function(reply) {
-            topicManager.updateLastReply(topic_id, reply._id, function(err, topic) {
-                if (err) {
-                    return next(err);
-                }
-                //发送at消息，并防止重复 at 作者
-                var newContent = linkedContent.replace('@' + topic.author.loginname + ' ', '');
-                at.sendMessageToMentionUsers(newContent, topic_id, current_user._id, reply._id);
-                ep.emit('reply_saved', reply);
-            });
-        }));
+        replyManager.newAndSave(content, topic_id, cu._id, ep.done('reply_saved'));
     });
-    topicManager.getTopicById(topic_id, ep.done('topic'));
-    at.linkUsers(content, ep.done('linkUsers'));
 
-    ep.all('reply_saved', function(reply) {
-            userManager.getUserById(current_user._id, ep.done(function(user) {
-                    user.reply_count += 1;
-                    user.save();
-                    req.session.user = user;
+    ep.all('topic', 'reply_saved', function(topic, reply) {
+        //发送at消息，并防止重复 at 作者
+        var newContent = content.replace('@' + topic.author.loginname + ' ', '');
+        at.sendMessageToMentionUsers(newContent, topic_id, cu._id, reply._id, ep.done('at'));
 
-                    ep.emit('reply', reply);
-                }));
-            });
+        userManager.getUserById(cu._id, ep.done('user_updated', function(user) {
+            user.reply_count += 1;
+            user.save();
+            req.session.user = user;
+        }));
+        topicManager.updateLastReply(topic_id, reply._id, ep.done('topic_updated'));
+    });
 
-    ep.all('reply', function(reply) {
+    ep.all('reply_saved', 'user_updated', 'topic_updated', function() {
         res.redirect('/topic/' + topic_id);
     });
+    topicManager.getTopicById(topic_id, ep.done('topic'));
 };
